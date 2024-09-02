ea9ba2e0d57bc9e7f85e3ec1d2190c2f353bf7f4
Mcube728 <boredAstronaut728@protonmail.com>
2024-09-02 14:51:03 +0530

    added posts, create posts

--- board/__init__.py
+++ board/__init__.py
@@ -1,7 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
-from board import pages
+
+from board import pages, posts
+
+load_dotenv()       # Load Environment Variables
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_prefixed_env()
     app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
     return app
