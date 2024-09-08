ded66f3ddf30e0977c1ed1cb6535b6d4530cfbf5
Mcube728 <boredAstronaut728@protonmail.com>
2024-09-08 15:45:52 +0530

    Added Comments

--- board/posts.py
+++ board/posts.py
@@ -35,3 +35,35 @@ def posts():
     posts_collection = db['posts']
     posts = posts_collection.find().sort('created', -1)     # -1 is descending order
     return render_template('posts/posts.html', posts=posts)
+
+@bp.route("/posts/<post_id>/add_reply", methods=('GET', 'POST'))
+def reply(post_id):
+    if request.method == 'POST':
+        author = request.form['author'] or 'Anonymous'
+        message = request.form['message']
+
+        if bool(message.strip()):   # Check for non empty strings
+            db = get_db()
+            replies_collection = db['replies']
+            post={
+                'post_id': ObjectId(post_id),
+                'author': author,
+                'message': message,
+                'created': datetime.now(timezone.utc)
+            }
+            replies_collection.insert_one(post)
+            current_app.logger.debug(f'New post by {author}')
+            flash(f'Thank you for your reply, {author}!', category='success')
+            return redirect(url_for('posts.replies', post_id=post_id))
+        else: 
+            flash('You need to post a message!', category='error')
+    return render_template('replies/create.html')
+
+@bp.route("/posts/<post_id>/replies",)
+def replies(post_id):
+    db = get_db()
+    replies_collection = db['replies']
+    posts_collection = db['posts']
+    post = posts_collection.find_one({"_id": ObjectId(post_id)})
+    replies = replies_collection.find().sort('created', -1)     # -1 is descending order
+    return render_template('replies/replies.html', replies=replies, post=post)
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,14 @@
 from board import create_app
 from waitress import serve
+import argparse
 
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=3000)
+    parser = argparse.ArgumentParser(description='starts the server.')
+    parser.add_argument('-d', '--debug', action='store_true', help='Runs the server in debug mode')
+    args = parser.parse_args()
+    if args.debug is None:
+        serve(app, host='0.0.0.0', port=3000)       
+    else: 
+        app.run(host='0.0.0.0', port=3000, debug=True)
